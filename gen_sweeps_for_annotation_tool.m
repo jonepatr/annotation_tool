151cf5825e9e9eb6a6995d33f27b5c6ac801676c
Fredrik Johansson <frejon@gmail.com>
2018-04-06 11:53:56 +0200

    Design, etc. Bug fix
    Major bug involving me keeping the repo on dropbox. I’ve included some
    input files in the repo for now, probably not not necessary. Also
    included a matlab script for generating input files.
    All we need now is to make sure the input files have a
    pseudo-random/unique filename so it won’t get overwritten

---
+++ gen_sweeps_for_annotation_tool.m
@@ -0,0 +1,177 @@
+%gen_sweeps_for_annotation_tool
+load=0;
+if load
+command = 'ls /mnt/squid/RO-C-RPCLAP-5-1*V0.7/*/*/*/*I1S.TAB';
+
+[status,command_output] = system(command);
+cell_listoffiles=textscan(command_output,'%s');
+%dataraw=  readAxS_prelim(cell_listoffiles{1,1});
+
+len_f=length(cell_listoffiles{1,1});
+
+end
+
+nrofsweeps=100;
+max_sweep=0;
+for i =1:nrofsweeps %bump up to 1000 soon
+    
+    ind_1(i)=1+floor(rand()*(len_f-1)+0.5);
+   
+    while isempty(dataraw(ind_1(i)).sweeps) %oh noes, its empty
+        ind_1(i)=1+floor(rand()*(len_f-1)+0.5); %try again
+    end
+    
+    lenS(i)=length(dataraw(ind_1(i)).sweeps(:,1));
+    ind_2(i)=1+floor(rand*(lenS(i)-1)+0.5);
+    
+    lenmax=length(dataraw(ind_1(i)).sweeps(1,:));
+    if lenmax>max_sweep
+        max_sweep = lenmax
+    end
+    
+    
+
+    
+end
+
+
+
+test = 1;
+if test    
+    sweep=[];
+    figure(1)
+    hold on;
+    for i = 1:10        
+        I=dataraw(ind_1(i)).sweeps(ind_2(i),:);
+        Vb= dataraw(ind_1(i)).bias_potentials;
+        plot(Vb,I)
+        grid on;
+    end
+    hold off;
+end
+
+%ind_1
+%lenS
+%ind_2
+fname='generatedsweeps_3.csv';
+twID = fopen(fname,'w');
+
+Dataarray=nan(nrofsweeps,max_sweep);
+for i = 1:nrofsweeps
+    leennnngth=length(dataraw(ind_1(i)).sweeps(ind_2(i),:));
+    Dataarray(i,1:leennnngth*2)=[dataraw(ind_1(i)).sweeps(ind_2(i),:) dataraw(ind_1(i)).bias_potentials.'];
+end
+    Dataarray(Dataarray==0)=-1000;
+    Dataarray(isnan(Dataarray))=-1000;
+
+for i = 1:nrofsweeps
+    i
+    b1 = fprintf(twID,'%s%s, %d',cell_listoffiles{1,1}{ind_1(i),1},dataraw(ind_1(i)).START_TIME_UTC{1,1},dataraw(ind_1(i)).qf(ind_2(i)));
+    b2 = fprintf(twID,', %14.7e',Dataarray(i,:));
+    %b2 = fprintf(twID,', %14.7e',dataraw(ind_1(i)).sweeps(ind_2(i),:).'); %
+    %b3 = fprintf(twID,', %14.7e',dataraw(ind_1(i)).bias_potentials.'); %some steps could be "NaN" values if LDL macro
+    b4 = fprintf(twID, '\r\n');
+    
+end
+
+fclose(twID);
+
+
+
+
+function dataraw = readAxS_prelim(list_of_ixsfiles)
+%dataraw takes a filepath to a file with paths to A1S or A2S files, outputs
+%it to a horrible massive struct called dataraw, to be polished later. It
+%also starts some stuff needed for iph0 calculations later.
+% dataraw massive struct ? la dataraw(:).t1 = many structs with arrays of
+% t1 from each A1S or A2S file specified in file filename
+% trID = fopen(filename);
+% scantemp=textscan(trID,'%s');
+% fclose(trID);
+% file1=scantemp{1,1};
+
+%dataraw=struct
+
+perc=0;
+for i = 1:length(list_of_ixsfiles)
+    
+
+    if mod(i,floor(length(list_of_ixsfiles)/10))==0
+        %    if mod(10,floor(100*i/length(list_of_axsfiles)))==0
+        
+%    if mod(i,floor(length(file1)/100)) == 1 %% Output counter routine for monitoring
+        fprintf(1,'%i percent of LAP axS list done\n',perc);
+        perc=perc+10;
+
+    end
+    
+
+    
+    
+    rfile =list_of_ixsfiles{i,1};
+    rfile3=rfile;
+    rfile3(end-6)='B';
+    
+    if exist(rfile,'file') && ~ismember(str2double(rfile(end-10:end-8)),[304;305;306;307;916;917;922;411]) %if file exists, and is not 304/307 fine sweeps
+        %macro 411 only gives problems for LAP2
+        
+        temp=lap_import(rfile);
+        %        %formatin = 'YYYY-mm-ddTHH:MM:SS'; temp.t1 = datenum(cspice_et2utc(cspice_str2et(temp.START_TIME_UTC),'ISOC',0),formatin);
+        %temp.t1 = irf_time(cell2mat(temp.START_TIME_UTC(:,1)),'utc>epoch');
+        temp.macroId(1:length(temp.qf),1)=str2double(rfile(end-10:end-8));
+        %temp.strID=strcat(rfile(end-51:end-48),'LAP',rfile2(end-5));
+        temp.data=[];
+        temp.STOP_TIME_UTC=[];
+        
+        %---- comment out this for now
+        
+
+        
+        temp3=lap_import(rfile3);
+        %temp.t1 = datenum(cspice_et2utc(cspice_str2et(temp.START_TIME_UTC),'ISOC',0),formatin);
+        temp.bias_potentials=temp3.bias_potentials;
+        %temp2.sample_times=temp3.sample_times;
+        
+%         if (temp.macroId(1) == 204 ||temp.macroId(1) == 926)
+%             
+%             %double some arrays, it's annoying, but whatevs.
+%             temp.curr(1:length(temp.t1),1)=nan; %% length(temp.t1)!!!
+%             temp.B(1:length(temp.t1),1)=nan;
+%             shit=temp.ion_slope;
+%             shit(1:length(shit)/2)=shit(1:2:end);% map every second value from start:0.5len.
+%             shit(length(shit)/2:end)=nan;%fill with nans. I don't really care anymore
+%             temp.ion_slope=shit;
+%             
+%             % special case, I'm ignoring the second part of the 204 sweep for now.
+%             %I wan't to isolate the first value close to -17, as that's the one that the AxS file is referring to
+%             % macro 204 has 208 steps, and 926 has 240 steps, so the first
+%             % 1:104 values should contain the "first part" of the sweep
+%             [checkthis,ind1] = sort(abs(temp2.bias_potentials(1:104)-V_query)); % find and sort values closest to -17.0V
+%                         
+%         else
+%             [checkthis,ind1] = sort(abs(temp2.bias_potentials-V_query)); % find and sort values closest to -17.0V
+%             
+%         end
+%         
+        %ind1=find( abs(temp2.bias_potentials+17.0) < epsilon) ; %Find potential near -17, within 1.0V
+        
+%         if checkthis(1) < epsilon %max 1V away from  17
+%             %        ind1=find(temp2.bias_potentials > -17.5 & temp2.bias_potentials < -16) ; %Find current from the sweeps at a specific bias voltage
+%             %if ~isempty(ind1)
+%             temp.curr(1:length(temp2.qf),1)=temp2.sweeps(:,ind1(1)); %length(temp2.qf)!!!
+%             temp.B(1:length(temp2.qf),1)=temp2.bias_potentials(ind1(1));
+%             
+%         else
+%             temp.curr(1:length(temp2.qf),1)=nan;
+%             temp.B(1:length(temp2.qf),1)=nan;
+%         end
+        dataraw(i) = temp; %Here's the write file  
+    else
+        fprintf(1,'skipping %s, i=%d\n',rfile,i);
+        %        rfile
+    end
+end
+
+
+
+end % function
--- server.py
+++ server.py
@@ -1,22 +1,25 @@
 from flask import Flask, render_template, request
-import csv
 import json
 import numpy as np
 import math
+import pandas as pd
 #not really necessary import, good for debugging:
 import matplotlib.pyplot as plt
-import pandas as pd
+import csv
 
 app = Flask(__name__)
 
 #output_list = []
-
-output_list = [None] * 10000
+#initialise output_list. This is ugly.. Enough for 2000 sweeps though.
+#output_list = [None] * 10000
+output_list = [None] * 2000
 
 
 #Read file once:
 #read file that might have inconsistent column numbers between rows. Fill with nans:
-df=pd.read_csv('RPCLAP_20160921_080404_710_to_patrik2.csv', sep=',',header=None,engine='python')
+#df=pd.read_csv('RPCLAP_20160921_080404_710_to_patrik2.csv', sep=',',header=None,engine='python')
+df=pd.read_csv('generatedsweeps_3.csv', sep=',',header=None,engine='python')
+global identifier
 
 #Windows hardcoded path
 #df=pd.read_csv('/Users/Fredrik/Dropbox (IRFU)/Sweepslave/RPCLAP_20160921_080404_710_to_patrik2.csv', sep=',',header=None,engine='python')
@@ -25,25 +28,53 @@ df=pd.read_csv('RPCLAP_20160921_080404_710_to_patrik2.csv', sep=',',header=None,
 #function that returns IV sweep (two arrays) for a row that may have some extra nan values
 def getIV(df,row):
     #remove nan values in df.values[row,:], save in row_array
-    row_array=df.values[row,~np.isnan(df.values[row,:])]
+    global identifier
+    identifier=df.loc[row,0]
+
+    #remove nan values in df.values[row,:], save in row_array
+    #row_array=df.values[row,pd.notnull(df.values[row,:])]
+    row_array=df.values[row,:]
+
+    row_array = np.delete(row_array, [0,1], 0); #delete column 0 & 1
+    #row_array2=row_array[row_array!=-1000]
+    row_array=row_array.astype('float')
+#    indb=np.absolute(row_array+1000)>1e-10
+    ind_end=max(np.where(np.absolute(row_array+1000)>1e-10)[0])#find last non-nan (actually not -1000) value
+    #print(ind_end)
+    row_array=row_array[0:ind_end-1];
+
+
+
+#    row_array=row_array[indb]
 
     #print ( df.values[row,:].size - row_array.size)
     #some index calculations
-    len=row_array.size;
+    leng=row_array.size;
     #print(len)
-    header_lines=3;
+    header_lines=0;
 
     i_start= header_lines;
-    i_end=int((len-header_lines)/2 +header_lines )
+    i_end=int((leng-header_lines)/2 +header_lines )
     v_start= i_end #something wierd here with python. calling e.g. X(1:4),X(4:5) I don't care
-    v_end= len
+    v_end= leng
+
+
+    I=row_array[i_start:i_end]
+    V=row_array[v_start:v_end]
+    mask = np.ones(len(I), dtype=bool)
+    mask[I==-1000] = False
+    V=V[mask]
+    I=I[mask]
+    #I=I.astype('float')
+    #V=V.astype('float')
+
    # print(row_array[0],row_array[1],row_array[2],row_array[3])
    # print(i_start)
    # print(i_end)
    # print(v_start)
-   # print(v_end)
+    #print(identifier)
     #return [I, V]
-    return [row_array[i_start:i_end],row_array[v_start:v_end]]
+    return [I,V]
 
 #read file that might have inconsistent column numbers between rows. Fill with nans:
 #import pandas as pd
@@ -68,40 +99,52 @@ def next_row(current_row):
 
     I,V = getIV(df,current_row) #Currents & potentials from file. Need to have some max row >= current_row condition here.
     # reading from csv...
+
+    #I=1e9*I
+    #indb=np.absolute(I+1000)>1e-10#remove any remaining NaN (-1000)
+    #V=V[indb+1]
+    #I=I[indb+1]
+
+#    V(np.isnan(I))=[]
+#    I(np.isnan(I))=[]
+
+
+
+    I=np.round(1e9*I*1e5)/1e5
+    V=np.round(V*1e5)/1e5
+    dI =np.gradient(I)
+    #dI =I
     x = sorted(np.random.rand(10)*10)
     y = np.random.rand(10)*10
 
-    data = [{'x': a, 'y': b} for a, b in zip(V, I*1e9)]
+    data = [{'x': a, 'y': b} for a, b in zip(V, I)]
+    #data2 = [{'x': a, 'y': b} for a, b in zip(V, dI)]
     return json.dumps({'data': data})
+
+    #return json.dumps({'data': data,'data2': data2})
 
 @app.route('/save_values/<int:row>', methods=['POST'])
 def save_values(row):
     print('Adding to csv...', row, request.form.get('Vph'), request.form.get('Vbar'), request.form.get('checker1'), request.form.get('checker2'))
 
+    global identifier
+    #prepare ouput, send to python list
     ind=(row-1)*5
     col1=request.form.get('Vph')
     col2=request.form.get('Vbar')
     col3=request.form.get('checker1')
     col4=request.form.get('checker2')
 
-    output_list[ind]=row
+    output_list[ind]=identifier
     output_list[ind+1]=col1
     output_list[ind+2]=col2
     output_list[ind+3]=col3
     output_list[ind+4]=col4
+    #output_list is kept up to date even if you go back and forwards in the tool
 
+    #convert output to dataframe, output all of it.
     df_out = pd.DataFrame(output_list)
     df_out.to_csv('output.csv', index=False)
-
-
-
-    #np.savetxt('data.csv', (row, request.form.get('Vph'), request.form.get('Vbar'),request.form.get('checker')), delimiter=',')
-
-   # np.savetxt('data.csv', (col1_array, col2_array, col3_array), delimiter=',')
-    #  with open('filnamn.csv', 'w') as f:
-  #      spamwriter = csv.writer(f,delimiter=',')
-  #      spamwriter.writerow(row + request.form.get('Vph') + request.form.get('Vbar') + request.form.get('checker'))
-    #spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
 
     return 'ok'
 
