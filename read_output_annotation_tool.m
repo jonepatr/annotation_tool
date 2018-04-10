46a7d4547255f1cc1688d603df3027e1141d42a0
Fredrik Johansson <frejon@gmail.com>
2018-04-10 11:04:19 +0200

    merge

--- read_output_annotation_tool.m
+++ read_output_annotation_tool.m
@@ -11,15 +11,30 @@ fclose(trID2);
 len = length(scantemp{1,1});
 out = [];
 j=0;
+
+substring='""';
+newstring='';
+scantemp{1,1} = struct_string_replace(scantemp{1,1},substring,newstring); %third party code
+
 for i = 2:5:len
     i
     j=j+1;
-    out.path{j,1}=scantemp{1,1}{i,1}(1:88);
-    out.UTC_START_TIME{j,1}=scantemp{1,1}{i,1}(89:end);
-    out.Vph(j)=str2double(scantemp{1,1}{i+1,1});
-    out.Vbar(j)=str2double(scantemp{1,1}{i+2,1});
-    out.check_disturb{j,1}= scantemp{1,1}{i+3,1};
-    out.check_calib{j,1}= scantemp{1,1}{i+3,1};
+    if ~isempty(scantemp{1,1}{i,1})
+        out.path{j,1}=scantemp{1,1}{i,1}(1:88);
+        out.UTC_START_TIME{j,1}=scantemp{1,1}{i,1}(89:end);
+        out.Vph(j)=str2double(scantemp{1,1}{i+1,1});
+        out.Vbar(j)=str2double(scantemp{1,1}{i+2,1});
+        out.check_disturb{j,1}= scantemp{1,1}{i+3,1};
+        out.check_calib{j,1}= scantemp{1,1}{i+3,1};
+    else
+        out.path{j,1}='';
+        out.UTC_START_TIME{j,1}='';
+        out.Vph(j)=nan;
+        out.Vbar(j)=nan;
+        out.check_disturb{j,1}='';
+        out.check_calib{j,1}='';
+    end
+        
 end
 
     
